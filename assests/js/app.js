e631c3538b990489c4d52b406d60ba819d01ca5d
PRASHANT NALKALLE <pnalkalle@gmail.com>
2026-04-30 17:00:13 +0530

    work on create  functionality

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,5 +1,9 @@
 const cl =console.log;
 const todocontainer = document.getElementById('todo-container')
+const todoform = document.getElementById('todoform')
+const todoitem = document.getElementById('todoitem')
+
+
 let todoArr = [
   {
     todoItem: "CSS",
@@ -18,7 +22,7 @@ let todoArr = [
 function todolist(arr){
     let result =``
 
-todoArr.forEach(ele => {
+arr.forEach(ele => {
     
  result += ` <li class="list-group-item d-flex justify-content-between">
 						<strong>${ele.todoItem}</strong>
@@ -34,4 +38,29 @@ todoArr.forEach(ele => {
 todocontainer.innerHTML=result
 }
 
+function onsubmithand(ele){
+  ele.preventDefault();
+  // cl('submited')
+  let newtodo ={
+    todoItem : todoitem.value,
+    todoId : Date.now().toString()
+  }
+  todoArr.push(newtodo)
+  let li = document.createElement('li')
+  li.className="list-group-item d-flex justify-content-between"
+
+  li.innerHTML =`<strong>${newtodo.todoItem}</strong>
+						<div>
+							<i type="button" class="fa-solid fa-pen-to-square fa-2x text-primary"></i>
+							<i type="button" class="fa-solid fa-trash fa-2x text-danger"></i>
+						</div>`
+
+  todocontainer.append(li)          
+  todoform.reset()
+
+  // cl(newtodo)
+}
+
 todolist(todoArr);
+
+todoform.addEventListener('submit',onsubmithand)
