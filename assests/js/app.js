2756b5cf9fe69224a04d70464af06521cab6660e
PRASHANT NALKALLE <pnalkalle@gmail.com>
2026-04-30 11:58:23 +0530

    work on functionality

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,1 +1,37 @@
 const cl =console.log;
+const todocontainer = document.getElementById('todo-container')
+let todoArr = [
+  {
+    todoItem: "CSS",
+    todoId: "2qwe12-231-231wd-ew112e",
+  },
+  {
+    todoItem: "JS & ES6",
+    todoId: "2qwe12-231-231wd-ew112e",
+  },
+  {
+    todoItem: "HTML",
+    todoId: "2qwe12-231-231wd-ew112e",
+  },
+];
+
+function todolist(arr){
+    let result =``
+
+todoArr.forEach(ele => {
+    
+ result += ` <li class="list-group-item d-flex justify-content-between">
+						<strong>${ele.todoItem}</strong>
+						<div>
+							<i type="button" class="fa-solid fa-pen-to-square fa-2x text-primary"></i>
+							<i type="button" class="fa-solid fa-trash fa-2x text-danger"></i>
+						</div>
+						
+					</li>
+            `
+})
+
+todocontainer.innerHTML=result
+}
+
+todolist(todoArr);
