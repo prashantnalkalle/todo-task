4b653ea9948b79373d1f455ee784df65b1b081b7
PRASHANT NALKALLE <pnalkalle@gmail.com>
2026-05-02 12:17:34 +0530

    work on remove functionality

--- assests/js/app.js
+++ assests/js/app.js
@@ -11,24 +11,25 @@ let todoArr = [
   },
   {
     todoItem: "JS & ES6",
-    todoId: "2qwe12-231-231wd-ew112e",
+    todoId: "2qwe12-231-231wd-ew1133",
   },
   {
     todoItem: "HTML",
-    todoId: "2qwe12-231-231wd-ew112e",
+    todoId: "2qwe12-231-231wd-ew11563",
   },
 ];
+
 
 function todolist(arr){
     let result =``
 
 arr.forEach(ele => {
     
- result += ` <li class="list-group-item d-flex justify-content-between">
+ result += ` <li class="list-group-item d-flex justify-content-between" id=${ele.todoId.toString()}>
 						<strong>${ele.todoItem}</strong>
 						<div>
 							<i type="button" class="fa-solid fa-pen-to-square fa-2x text-primary"></i>
-							<i type="button" class="fa-solid fa-trash fa-2x text-danger"></i>
+							<i type="button" class="fa-solid fa-trash fa-2x text-danger" onclick="onRemove(this)"></i>
 						</div>
 						
 					</li>
@@ -46,19 +47,45 @@ function onsubmithand(ele){
   }
   todoArr.push(newtodo)
   let li = document.createElement('li')
-  li.className="list-group-item d-flex justify-content-between"
-
+  li.className=`list-group-item d-flex justify-content-between`
+  li.id = newtodo.todoId.toString();
   li.innerHTML =`<strong>${newtodo.todoItem}</strong>
 						<div>
 							<i type="button" class="fa-solid fa-pen-to-square fa-2x text-primary"></i>
-							<i type="button" class="fa-solid fa-trash fa-2x text-danger"></i>
+							<i type="button" class="fa-solid fa-trash fa-2x text-danger" onclick="onRemove(this)"></i>
 						</div>`
 
   todocontainer.append(li)          
   todoform.reset()
-
+  Swal.fire({
+  title: `New ToDo item ${newtodo.todoItem} is added successfully !!!!`,
+  icon: "success",
+  timer : 3000
+  });
  
 }
+
+function onRemove(ele){
+ let RemoveId = ele.closest("li").id;
+
+ let getIndex = todoArr.findIndex(ele =>{
+  return ele.todoId == RemoveId
+ })
+
+ let removeitem = todoArr.splice(getIndex,1)
+ ele.closest('li').remove()
+
+ Swal.fire({
+  title : `Todo Item ${removeitem[0].todoItem} removed successfully !!!`,
+  icon : 'success',
+  timer : 3000
+ })
+
+}
+
+
+
+
 
 todolist(todoArr);
 
