9ae812ecc55b157d49bcbf45a51f4225dfa809c5
PRASHANT NALKALLE <pnalkalle@gmail.com>
2026-05-03 18:21:02 +0530

    work on edit and update functionality

--- assests/js/app.js
+++ assests/js/app.js
@@ -2,7 +2,13 @@ const cl =console.log;
 const todocontainer = document.getElementById('todo-container')
 const todoform = document.getElementById('todoform')
 const todoitem = document.getElementById('todoitem')
+const updatetodo = document.getElementById('updatetodo')
+const addtodo = document.getElementById('addtodo')
 
+
+
+// cl(addtodo)
+// cl(updatetodo)
 
 let todoArr = [
   {
@@ -28,7 +34,7 @@ arr.forEach(ele => {
  result += ` <li class="list-group-item d-flex justify-content-between" id=${ele.todoId.toString()}>
 						<strong>${ele.todoItem}</strong>
 						<div>
-							<i type="button" class="fa-solid fa-pen-to-square fa-2x text-primary"></i>
+							<i type="button" class="fa-solid fa-pen-to-square fa-2x text-primary " onclick="onEdit(this)"></i>
 							<i type="button" class="fa-solid fa-trash fa-2x text-danger" onclick="onRemove(this)"></i>
 						</div>
 						
@@ -51,7 +57,7 @@ function onsubmithand(ele){
   li.id = newtodo.todoId.toString();
   li.innerHTML =`<strong>${newtodo.todoItem}</strong>
 						<div>
-							<i type="button" class="fa-solid fa-pen-to-square fa-2x text-primary"></i>
+							<i type="button" class="fa-solid fa-pen-to-square fa-2x text-primary" onclick="onEdit(this)"></i>
 							<i type="button" class="fa-solid fa-trash fa-2x text-danger" onclick="onRemove(this)"></i>
 						</div>`
 
@@ -82,11 +88,52 @@ function onRemove(ele){
  })
 
 }
+let EditId;
+function onEdit(ele){
+   EditId = ele.closest('li').id;
+
+  let Edit_Obj = todoArr.find(ele => {
+    return ele.todoId == EditId
+  })
+
+  // cl(Edit_Obj)
+  todoitem.value = Edit_Obj.todoItem;
+  addtodo.classList.add('d-none');
+  // cl(addtodo.classList)
+  updatetodo.classList.remove('d-none');
+}
+
+function onUpdatehandl(ele){
+let update_id = EditId;
+let updatedToDo ={
+  todoItem : todoitem.value,
+  todoId : update_id
+}
+
+let index = todoArr.findIndex(ele => ele.todoId == update_id)
+
+todoArr[index] = updatedToDo
+
+let li = document.getElementById(update_id).firstElementChild
+
+li.innerText = updatedToDo.todoItem
+
+todoform.reset();
+addtodo.classList.remove('d-none')
+updatetodo.classList.add('d-none')
+
+ Swal.fire({
+  title : `Todo Item ${updatedToDo.todoItem} updated successfully !!!`,
+  icon : 'success',
+  timer : 3000
+ })
 
 
+}
 
 
 
 todolist(todoArr);
 
 todoform.addEventListener('submit',onsubmithand)
+updatetodo.addEventListener('click',onUpdatehandl)
